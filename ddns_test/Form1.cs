6cd2ecdca5f346f741e3f3215889cf92a28b5569
unknown <ever43@gmail.com>
2021-05-03 13:15:46 +0900

    Init Version

---
+++ ddns_test/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+
+namespace ddns_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IPHostEntry host;
+
+            host = Dns.GetHostEntry(textBox1.Text);
+
+            textBox2.AppendText("GetHostEntry:" + textBox1.Text + Environment.NewLine);
+
+            textBox2.AppendText(host.AddressList.Length.ToString()+Environment.NewLine);
+            foreach (IPAddress ip in host.AddressList)
+            {
+                textBox2.AppendText(ip.ToString() + " / ");
+            } 
+        }
+    }
+}
---
+++ git init.bat
@@ -0,0 +1,8 @@
+git init
+git add .
+git commit -m "Init Version"
+git remote add origin https://github.com/oSiny84/VSTools_ddns_test.git
+git remote -v
+git branch -M main
+git push origin main
+
